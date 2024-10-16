e5bf66e0f93aa0a09c6f47fa8936464c098adc9f
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-10-16 18:41:57 -0500

    Import section components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { FaGithub, FaStackOverflow, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { HiOutlineDownload } from "react-icons/hi";
-import { BsStars } from "react-icons/bs";
+import AboutSection from "../components/AboutSection";
+import ResumeSection from "../components/ResumeSection";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 const Home = () => {
     return (
@@ -32,22 +35,10 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section id="about" className="flex bg-zinc-800 px-4 py-16 justify-center items-center">
-                <div className="flex justify-center max-w-4xl">
-                    <img src="/src/assets/portrait.jpg" className="rounded-3xl w-48"></img>
-                    <div className="flex flex-col pl-24">
-                        <h2 className="text-slate-100">About Me</h2>
-                        <p className="text-slate-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum distinctio fugit velit dolor maxime inventore a cum dolorum dolorem labore animi doloribus repudiandae voluptate nesciunt vel, reprehenderit corporis! Sapiente. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quam perferendis, commodi, perspiciatis aliquid culpa facilis consequatur voluptas repudiandae eius saepe? Impedit fugiat, minus in reprehenderit dolor ipsa quos illum? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit</p>
-                        <div className="flex items-center">
-                            <BsStars size={16} className="text-slate-100 mr-1" />
-                            <span className="text-slate-100">Interests: </span>
-                            <p className="ml-2 text-slate-300">Lorem, Ipsum, Piano</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section id="resume" className="bg-zinc-50">Resume</section>
-            <section id="contact" className="bg-zinc-500">Contact</section>
+            <AboutSection />
+            <ResumeSection />
+            <ContactSection />
+            <Footer />
         </div>
     );
 };

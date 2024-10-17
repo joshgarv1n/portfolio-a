688418c26cd82bcca2a23f0d7fe8330987ab1939
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-10-17 18:29:39 -0500

    Footer initial design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 const Footer = () => {
     return (
-        <section id="footer" className="bg-zinc-50">Footer</section>
+        <section id="footer" className="bg-neutral-900 text-neutral-700 font-montserrat">
+            <div className="flex justify-center text-sm p-2">
+                <p>&copy; 2024 Josh Garvin</p>
+            </div>
+        </section>
     );
 };
 

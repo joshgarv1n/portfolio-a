1da5488ef3f77903955ecf34bf744438a9025bda
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-10-17 18:49:10 -0500

    Update resume link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
                         </a>
                     </div>
                     <div className="flex justify-center space-x-4 mt-6">
-                        <a href="" download className="flex items-center px-4 py-2 border-2 border-orange-500 text-neutral-50 rounded-full hover:bg-slate-400 hover:bg-opacity-50 hover:text-neutral-50 transition duration-300">
+                        <a href="./public/resume.pdf" download className="flex items-center px-4 py-2 border-2 border-orange-500 text-neutral-50 rounded-full hover:bg-slate-400 hover:bg-opacity-50 hover:text-neutral-50 transition duration-300">
                             Resume <HiOutlineDownload size={20} className="ml-2" />
                         </a>
                         <a href="#contact" className="px-4 py-2 border-2 border-white text-white rounded-full hover:bg-slate-400 hover:bg-opacity-50 hover:text-white transition duration-300">

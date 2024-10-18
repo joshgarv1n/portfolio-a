9884d168570b53133a555cba7c0c1aca8f8b6d68
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-10-17 21:33:32 -0500

    Replace form with ContactForm

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,6 +2,7 @@ import { LuMail } from "react-icons/lu";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import ContactForm from "./ContactForm";
 
 const ContactSection = () => {
     return (
@@ -16,15 +17,9 @@ const ContactSection = () => {
                     {/* Row 2: Form and Social Media */}
                     <div className="flex w-full gap-6">
                         {/* Contact Form */}
-                        <form className="w-1/2">
-                            <input className="bg-neutral-700 w-full rounded-md min-h-10 border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 placeholder:text-sm px-4 mb-4" placeholder="Name" required />
+                        <ContactForm />
 
-                            <input className="bg-neutral-700 w-full rounded-md min-h-10 border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 placeholder:text-sm px-4 mb-4" placeholder="Email" required />
-
-                            <textarea className="bg-neutral-700 w-full rounded-md min-h-10 border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 placeholder:text-sm px-4 py-2 mb-4" placeholder="Message" rows="6" required />
-
-                            <button className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm shadow-md focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800 hover:bg-stone-800">Send Message</button>
-                        </form>
+                        {/* Socials */}
                         <div className="flex flex-col w-1/2 gap-y-4">
                             <p>Thanks for visiting! Feel free to leave a message here, explore my projects, or download my resume.</p>
                             <div className="flex items-center gap-x-3">

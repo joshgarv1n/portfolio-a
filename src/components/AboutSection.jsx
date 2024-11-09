7eb264988a90ac84ae3624b61ac187a4a6fa08f9
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-11-09 10:40:30 -0600

    Update about text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
                 <img src="/portrait.jpg" className="rounded-3xl w-40 sm:w-48 max-h-56 mb-6 md:mb-0"></img>
                 <div className="flex flex-col p-0 sm:p-4 md:pl-24 font-montserrat">
                     <h2 className="text-neutral-100 text-2xl font-medium">About Me</h2>
-                    <p className="text-neutral-300 py-4">Hi! I’m Josh, a computer science student at the University of Houston and Software Engineer currently working in Automation. I’m passionate about using technology to solve real-world problems, whether that’s building effective automation solutions or developing robust, user-friendly applications. I enjoy working with tools like React, Python, and Node.js to create projects that are both functional and fun. My work has taught me to always look for smarter, more efficient ways to tackle challenges, and I love the continuous learning that comes with it.</p>
+                    <p className="text-neutral-300 py-4">Hi! I’m Josh, a computer science student at the University of Houston and Software Engineer currently working in Automation. I’m passionate about using technology to solve real-world problems, whether that’s building effective automation solutions or developing robust, user-friendly applications. I enjoy working with tools like React and Node.js to create projects that are both functional and fun. My work has taught me to always look for smarter, more efficient ways to tackle challenges, and I love the continuous learning that comes with it.</p>
                     <div className="flex items-center">
                         <BsStars size={16} className="text-slate-100 mr-1" />
                         <span className="text-neutral-100 font-medium">Interests: </span>

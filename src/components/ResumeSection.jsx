655854e9c5daaecee82345fe0e834cdba9af9df0
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-11-09 10:34:13 -0600

    Update skills, add work position, fix formatting

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -8,20 +8,25 @@ const ResumeSection = () => {
                         <h2 className="font-bold text-xl text-neutral-800 uppercase border-b-2 border-orange-500 pb-1 inline-block">Education</h2>
                     </div>
                 </div>
-                <div className="flex flex-col items-center md:items-start lg:w-3/4">
-                    <div className="pb-8">
+                <div className="flex flex-col items-center md:items-start md:w-3/4 gap-y-8">
+                    <div className="flex flex-col items-center md:items-start">
                         <h2 className="font-semibold text-center md:text-start text-xl text-neutral-700">BS Computer Science</h2>
+                        <div>
+                            <span className="italic text-sm">Minor in Mathematics</span>
+                        </div>
                         <div className="flex items-center justify-start gap-x-2 text-sm">
                             <span>University of Houston</span>
                             <span>•</span>
                             <span>December 2025</span>
                         </div>
                     </div>
-                    <h2 className="font-semibold text-xl text-neutral-700">AS Computer Science</h2>
-                    <div className="flex items-center justify-start gap-x-2 text-sm">
-                        <span>Austin Community College</span>
-                        <span>•</span>
-                        <span>December 2023</span>
+                    <div className="flex flex-col items-center md:items-start">
+                        <h2 className="font-semibold text-xl text-neutral-700">AS Computer Science</h2>
+                        <div className="flex items-center justify-start gap-x-2 text-sm">
+                            <span>Austin Community College</span>
+                            <span>•</span>
+                            <span>December 2023</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,14 +39,26 @@ const ResumeSection = () => {
                         <h2 className="font-bold text-xl text-neutral-800 uppercase border-b-2 border-orange-500 pb-1 inline-block">Work</h2>
                     </div>
                 </div>
-                <div className="flex flex-col items-center md:items-start lg:w-3/4">
-                    <h2 className="font-semibold text-center md:text-start text-xl text-neutral-700">International Game Technology IGT</h2>
-                    <div className="flex items-center justify-start gap-x-2 text-sm pb-5">
-                        <span>Computer Operator I</span>
-                        <span>•</span>
-                        <span>Jan 2020 - Sept 2021</span>
+                <div className="flex flex-col items-center md:items-start md:w-3/4 gap-y-8">
+                    <div className="flex flex-col items-center md:items-start">
+                        <h2 className="font-semibold text-center md:text-start text-xl text-neutral-700">International Game Technology IGT</h2>
+                        <div className="flex items-center justify-start gap-x-2 text-sm pb-5">
+                            <span>Software Engineer</span>
+                            <span>•</span>
+                            <span>Nov 2024 - Present</span>
+                        </div>
+                        <p className="text-center md:text-start">As a member of the Automation Team, I work to design and develop new and innovative automated solutions to improve operations company-wide.</p>
                     </div>
-                    <p>Adhered to operational checklists ensuring accurate execution of daily processing, performed troubleshooting of systems and monitored networks for anomalies. Worked to develop strong and effective relationships with our customers.</p>
+                    
+                    <div className="flex flex-col items-center md:items-start">
+                        <h2 className="font-semibold text-center md:text-start text-xl text-neutral-700">International Game Technology IGT</h2>
+                        <div className="flex items-center justify-start gap-x-2 text-sm pb-5">
+                            <span>Computer Operator</span>
+                            <span>•</span>
+                            <span>Jan 2020 - Sept 2021</span>
+                        </div>
+                        <p className="text-center md:text-start">Adhered to operational checklists ensuring accurate execution of daily processing, performed troubleshooting of systems and monitored networks for anomalies. Worked to develop strong and effective relationships with our customers.</p>
+                    </div>
                 </div>
             </div>
             
@@ -64,50 +81,29 @@ const ResumeSection = () => {
                                 <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">React</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-10/12"></div>
+                                        <div className="h-full rounded-full bg-orange-400 w-4/5"></div>
                                     </div>
                                 </div>
                                 {/* Tailwind */}
                                 <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">Tailwind</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-10/12"></div>
+                                        <div className="h-full rounded-full bg-orange-400 w-4/5"></div>
                                     </div>
                                 </div>
                                 {/* Vite */}
                                 <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">Vite</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-9/12"></div>
+                                        <div className="h-full rounded-full bg-orange-400 w-4/5"></div>
                                     </div>
-                                </div>
-                                {/* Typescript */}
-                                <div className="flex flex-col w-full">
-                                    <span className="ml-2 text-sm">Typescript</span>
-                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-1/2"></div>
-                                    </div>
-                                </div>                                    
+                                </div>                                   
                             </div>
                         </div>
 
                         <div className="flex flex-col items-center md:w-1/2">
                             <h2 className="font-semibold text-lg text-neutral-700">Backend development</h2>
                             <div className="flex flex-col gap-y-2 items-start w-full">
-                                {/* Python */}
-                                <div className="flex flex-col w-full">
-                                    <span className="ml-2 text-sm">Python</span>
-                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-10/12"></div>
-                                    </div>
-                                </div>
-                                {/* C++ */}
-                                <div className="flex flex-col w-full">
-                                    <span className="ml-2 text-sm">C++</span>
-                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-10/12"></div>
-                                    </div>
-                                </div>
                                 {/* Node.js */}
                                 <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">Node.js</span>
@@ -121,28 +117,42 @@ const ResumeSection = () => {
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
                                         <div className="h-full rounded-full bg-orange-400 w-3/4"></div>
                                     </div>
+                                </div>            
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Row 2: Languages and Technologies */}
+                    <div className="flex flex-col md:flex-row justify-between gap-y-6 md:gap-y-0 md:gap-x-6 mb-8">
+                        <div className="flex flex-col items-center md:w-1/2">
+                            <h2 className="font-semibold text-lg text-neutral-700">Languages</h2>
+                            <div className="flex flex-col gap-y-2 items-start w-full">
+                                {/* Python */}
+                                <div className="flex flex-col w-full">
+                                    <span className="ml-2 text-sm">Python</span>
+                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
+                                        <div className="h-full rounded-full bg-orange-400 w-5/6"></div>
+                                    </div>
+                                </div>
+                                {/* C++ */}
+                                <div className="flex flex-col w-full">
+                                    <span className="ml-2 text-sm">C++</span>
+                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
+                                        <div className="h-full rounded-full bg-orange-400 w-5/6"></div>
+                                    </div>
+                                </div>
+                                {/* JavaScript */}
+                                <div className="flex flex-col w-full">
+                                    <span className="ml-2 text-sm">JavaScript</span>
+                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
+                                        <div className="h-full rounded-full bg-orange-400 w-3/4"></div>
+                                    </div>
                                 </div>
                                 {/* R */}
                                 <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">R</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
                                         <div className="h-full rounded-full bg-orange-400 w-2/3"></div>
-                                    </div>
-                                </div>            
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Row 2: Databases and Technologies */}
-                    <div className="flex flex-col md:flex-row justify-between gap-y-6 md:gap-y-0 md:gap-x-6 mb-8">
-                        <div className="flex flex-col items-center md:w-1/2">
-                            <h2 className="font-semibold text-lg text-neutral-700">Databases</h2>
-                            <div className="flex flex-col gap-y-2 items-start w-full">
-                                {/* SQLite */}
-                                <div className="flex flex-col w-full">
-                                    <span className="ml-2 text-sm">SQLite</span>
-                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-10/12"></div>
                                     </div>
                                 </div>                                   
                             </div>
@@ -155,18 +165,41 @@ const ResumeSection = () => {
                                <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">Linux</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-3/4"></div>
+                                        <div className="h-full rounded-full bg-orange-400 w-4/5"></div>
                                     </div>
                                 </div>
                                 {/* Git */}
                                <div className="flex flex-col w-full">
                                     <span className="ml-2 text-sm">Git</span>
                                     <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
-                                        <div className="h-full rounded-full bg-orange-400 w-2/3"></div>
+                                        <div className="h-full rounded-full bg-orange-400 w-3/4"></div>
+                                    </div>
+                                </div>
+                                {/* Vercel */}
+                                <div className="flex flex-col w-full">
+                                    <span className="ml-2 text-sm">Vercel</span>
+                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
+                                        <div className="h-full rounded-full bg-orange-400 w-3/4"></div>
                                     </div>
                                 </div>
                             </div>
                         </div>                  
+                    </div>
+
+                    {/* Row 3: Databases */}
+                    <div className="flex flex-col md:flex-row justify-between gap-y-6 md:gap-y-0 md:gap-x-6 mb-8">
+                        <div className="flex flex-col items-center md:w-1/2">
+                            <h2 className="font-semibold text-lg text-neutral-700">Databases</h2>
+                            <div className="flex flex-col gap-y-2 items-start w-full">
+                                {/* SQLite */}
+                                <div className="flex flex-col w-full">
+                                    <span className="ml-2 text-sm">SQLite</span>
+                                    <div className="h-5 overflow-hidden rounded-full bg-neutral-300">
+                                        <div className="h-full rounded-full bg-orange-400 w-3/5"></div>
+                                    </div>
+                                </div>                                   
+                            </div>
+                        </div>                 
                     </div>
                 </div>
             </div>
